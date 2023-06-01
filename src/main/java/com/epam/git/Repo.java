3d0622d5b2f34ab79e438115a58a2d3954f3cc56
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-01 22:18:07 +0300

    Complited the tasks

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "git@github.com:mr-Krasnovsky/task2_3.git";
 }
